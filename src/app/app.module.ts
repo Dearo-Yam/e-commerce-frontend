548bfb2c61cc3a2a2061b6064fb70695489fd2d4
Dearo-Yam <70773145+Dearo-Yam@users.noreply.github.com>
2022-09-22 16:12:17 -0700

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { TotalOrderShippedComponent } from './total-order-shipped/total-order-sh
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { PendingComponent } from './pending/pending.component';
+import { LoginComponent } from './login/login.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {  GoogleLoginProvider } from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -18,14 +21,31 @@ import { PendingComponent } from './pending/pending.component';
     TotalOrderShippedComponent,
     NavBarComponent,
     HomeComponent,
-    PendingComponent
+    PendingComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
+    
   ],
-  providers: [],
+  providers: [ {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: true,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com'
+          ),
+        }
+      ],
+    } as SocialAuthServiceConfig,
+  },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
