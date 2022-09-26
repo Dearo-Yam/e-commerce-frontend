98f96b3145717554cd893f5493610b34cbdf9fae
eagagas1 <eagagas@nisum.com>
2022-09-26 15:25:47 -0700

    added some things

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../order';
 import { OrderService } from '../order.service';
+import { __param } from 'tslib';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-table',
