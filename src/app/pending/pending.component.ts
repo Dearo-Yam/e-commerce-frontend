02443ee60128269a8e6856d662191d246c48535d
huangchuang0102 <huangchuang0102@gmail.com>
2022-09-26 18:32:18 -0700

    add pagnition for pending and shipped order

--- src/app/pending/pending.component.ts
+++ src/app/pending/pending.component.ts
@@ -9,13 +9,31 @@ import { OrdersService } from '../orders.service';
   styleUrls: ['./pending.component.css']
 })
 export class PendingComponent implements OnInit {
-orders:any;
+  orders:any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 10;
+  tableSizes: any = [3, 6, 9, 12];
   constructor(private _ordersService: OrdersService) { }
 
   ngOnInit(): void {
     this._ordersService.getPendingOrders().subscribe(orders =>{
       this.orders = orders;
     })
+  }
+
+  getOrders(): void {
+    this._ordersService.getPendingOrders()
+    .subscribe(orders => this.orders = orders);
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.getOrders();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.getOrders();
   }
 
 }
