ef90bceafba0698cb777b787989d45929142397c
EdwinTran <70764419+EdwinTran@users.noreply.github.com>
2022-09-28 11:33:16 -0700

    Changed front-end implementation for daily orders shipped

--- src/app/metrics/metrics.component.ts
+++ src/app/metrics/metrics.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../orders.service';
 
@@ -10,36 +11,59 @@ export class MetricsComponent implements OnInit {
   totalOrdersShipped: any;
   avgTime: any;
   topSellingItems: any;
-  weeklyShipping: any;
+  weeklyShipping = [];
   totalWeeklyShipped: any;
+  weekDates: string[] = [];
 
   constructor(private _ordersService: OrdersService) { }
 
   ngOnInit(): void {
+    this.refreshData();
+    this.getWeekDates();
+
+    setInterval(() => {
+      this.refreshData();
+    }, 5000);
+  }
+
+  refreshData(): void {
     this.getTotalOrdersShipped();
     this.getAvgTimeToShip();
     this.getTopSellingItems();
     this.getWeeklyShipping();
-
-    setInterval(() => {
-      this.getTotalOrdersShipped();
-      this.getAvgTimeToShip();
-      this.getTopSellingItems();
-      this.getWeeklyShipping();
-    }, 5000);
   }
 
   getSum() : number {
     let sum = 0;
     for(let i = 0; i < this.weeklyShipping.length; i++) {
-      sum += this.weeklyShipping[i][1];
+      sum += this.weeklyShipping[i];
     }
     return sum;
   }
 
+  getWeekDates() {
+    let currDayOfWeek = new Date().getDay();
+
+    let startOfWeek = new Date();
+    startOfWeek.setDate(startOfWeek.getDate() - currDayOfWeek + 1);
+    
+    for(let i = 1; i < 6; i++) {
+      if(i <= currDayOfWeek) {
+        this.weekDates[i - 1] = (startOfWeek.getMonth() + 1) + "/" + startOfWeek.getDate();
+      }
+      else {
+        let previousWeek = new Date(startOfWeek);
+        previousWeek.setDate(previousWeek.getDate() - 7);
+        this.weekDates[i - 1] = (previousWeek.getMonth() + 1) + "/" + previousWeek.getDate();
+      }
+      
+      startOfWeek.setDate(startOfWeek.getDate() + 1);
+    }
+  }
+
   getWeeklyShipping() {
     this._ordersService.getWeeklyShipping().subscribe(weeklyShipping => {
-      this.weeklyShipping = weeklyShipping;
+      this.weeklyShipping = weeklyShipping[0];
     })
   }
 
